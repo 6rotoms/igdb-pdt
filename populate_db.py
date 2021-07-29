58f9a7464107fd669c52db4c3ad21e2873acb173
zrui98 <ciguatera.z.rui@gmail.com>
2021-07-29 19:56:22 -0400

    address flake8 comments and clean up output

--- populate_db.py
+++ populate_db.py
@@ -12,7 +12,7 @@ import os
 from datetime import datetime
 import time
 
-BASE_URL='https://api.igdb.com/v4/games'
+BASE_URL = 'https://api.igdb.com/v4/games'
 CLIENT_ID = ''
 CLIENT_SECRET = ''
 REDIS_HOSTNAME = 'localhost'
@@ -43,7 +43,7 @@ async def set_authorization(
 ) -> dict:
     if not (CLIENT_ID and CLIENT_SECRET):
         print('CLIENT_ID and CLIENT_SECRET environment variables not set!')
-        return;
+        return
     url = 'https://id.twitch.tv/oauth2/token?client_id=%s&client_secret=%s&grant_type=client_credentials' % (CLIENT_ID, CLIENT_SECRET)
     resp = await session.post(url=url)
     data = await resp.json()
@@ -80,6 +80,7 @@ def get_cover(data: dict):
         return data['cover']['url'].replace('t_thumb', 't_cover_big')
     return ''
 
+
 async def fetch_games():
     async with aiohttp.ClientSession() as session:
         await set_authorization(session=session)
@@ -96,22 +97,22 @@ async def fetch_games():
                 time.sleep(5000/1000000000.0)
                 new_time = time.time_ns()
             last_time = new_time
-            tasks = []
-            for i in range(0, 2000, 500):
-                tasks.append(get_games(session=session, offset=i, max_id=max_id))
+            tasks = [get_games(session=session, offset=i, max_id=max_id) for i in range(0, 2000, 500)]
             new_data = await asyncio.gather(*tasks, return_exceptions=True)
             new_data = list(itertools.chain(*new_data))
             max_entry = max(new_data, key=lambda d: d['id'])
             max_id = int(max_entry['id'])
-            new_data = {p['slug']: {
-                'name': p['name'],
-                'alt_names': [*map(lambda v: v.get('name', ''), p.get('alternative_names', []))],
-                'summary': p.get('summary', False) or '',
-                'thumb':  p.get('cover', {}).get('url', '') or '',
-                'cover': get_cover(p),
-            } for p in new_data}
+            new_data = {
+                p['slug']: {
+                    'name': p['name'],
+                    'alt_names': [*map(lambda v: v.get('name', ''), p.get('alternative_names', []))],
+                    'summary': p.get('summary', False) or '',
+                    'thumb':  p.get('cover', {}).get('url', '') or '',
+                    'cover': get_cover(p),
+                }
+                for p in new_data}
             data = {**data, **new_data}
-        return json.dumps(data, indent=4)
+        return data
 
 
 def load_mock_data():
@@ -135,14 +136,14 @@ def cache_to_redis(data: dict):
                                 TextField('cover', weight=0),
                                 TextField('thumb', weight=0))
             indexCreated = True
-        except:
-            print('Failed to create index, retrying')
+        except Exception:
+            print('Failed to create index, retrying %s')
             time.sleep(3)
 
     for k, v in data.items():
         client.add_document(k,
                             name=v['name'],
-                            **{'alt_name_%d' % i:n for i, n in enumerate(v['alt_names'])},
+                            **{'alt_name_%d' % i: n for i, n in enumerate(v['alt_names'])},
                             cover=v['cover'],
                             thumb=v['thumb'],
                             summary=v['summary'])
@@ -150,13 +151,16 @@ def cache_to_redis(data: dict):
 
 
 def main(args: dict):
-    print('igdb redis updating: ', datetime.now())
+    if (not args.output):
+        print('igdb redis updating: ', datetime.now())
     if IGDB_SRC == 'MOCK' or args.mock:
         data = load_mock_data()
     else:
         data = asyncio.run(fetch_games())
     if args.output:
-        print(data)
+        print(json.dumps(data, indent=4))
+    else:
+        print('call complete. fetched %d games' % (len(data)))
     if args.persist:
         cache_to_redis(data=data)
 
